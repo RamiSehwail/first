37f4379911693c41b23d31638733f43292b04a43
RamiSehwail <143724265+RamiSehwail@users.noreply.github.com>
2023-09-03 19:01:11 +0300

    first ever upload

---
+++ first.py
@@ -0,0 +1,16 @@
+# basic teturial
+ 
+
+def basic() :
+    var1=input("hi")
+
+    print("this is a vs code teturial")
+    print ("so first you need to know how to print so you write print()")
+    var2=input("try it:- ")
+    if var2 == "print()":
+        print ("that is wright")
+    else :
+        var3=input("try again")
+    print("1st code is done congrats")
+
+basic()
